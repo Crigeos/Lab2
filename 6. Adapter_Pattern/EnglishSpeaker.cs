98dc181dfe34fd7e1b903397d3a7740c09b06368
Crigeos <56398383+Crigeos@users.noreply.github.com>
2022-04-22 12:15:01 +0300

    Add files via upload

---
+++ 6. Adapter_Pattern/EnglishSpeaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Pattern
+{
+    class EnglishSpeaker : iSpeaker
+    {
+        private string Name { get; set; }
+        public EnglishSpeaker(string name)
+        {
+            Name = name;
+        }
+        public string Answer(string text)
+        {
+               
+        }
+
+        public string Question(string text)
+        {
+            
+        }
+    }
+}
---
+++ 6. Adapter_Pattern/iSpeaker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Pattern
+{
+    interface iSpeaker
+    {
+        string Question(string text);
+        string Answer(string text);
+    }
+}
